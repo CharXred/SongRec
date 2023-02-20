12348fbaf6c6fc27a9836dd825d9df6c7dcb76cb
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2023-02-20 15:40:01 +0300

    Add error fallback

--- chartex-radio/src/main.rs
+++ chartex-radio/src/main.rs
@@ -1,9 +1,11 @@
 use std::fs;
 use std::time::{Duration, Instant};
 
+use anyhow::anyhow;
 use argh::FromArgs;
 use chrono::Utc;
 use futures_util::StreamExt;
+use reqwest::Client;
 use serde_json::Value;
 use songrec::fingerprinting::algorithm::SignatureGenerator;
 use songrec::fingerprinting::communication::recognize_song_from_signature;
@@ -30,26 +32,14 @@ struct Args {
     stream_file: String,
 }
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    let args: Args = argh::from_env();
-    let client = reqwest::Client::builder()
-        .user_agent(APP_USER_AGENT)
-        .timeout(Duration::from_secs(30))
-        .pool_max_idle_per_host(0)
-        .build()?;
-    let mut stream = reqwest::Client::builder()
-        .http2_keep_alive_while_idle(true)
-        .pool_idle_timeout(None)
-        .pool_max_idle_per_host(usize::MAX)
-        .build()?
+async fn start(args: &Args, client: &Client, stream_client: &Client) -> anyhow::Result<()> {
+    let mut stream = stream_client
         .get(&args.station)
         .send()
         .await?
         .bytes_stream();
     let mut chunks = Vec::<u8>::new();
     let mut time = Instant::now();
-    println!("Starting...");
     while let Some(chunk) = stream.next().await {
         let chunk = chunk?;
         chunks.extend(chunk.as_ref().to_vec());
@@ -59,40 +49,60 @@ async fn main() -> anyhow::Result<()> {
             time = Instant::now();
         }
         fs::write(&args.stream_file, &chunks)?;
-        match SignatureGenerator::make_signature_from_file(&args.stream_file) {
-            Ok(signature) => match recognize_song_from_signature(&signature) {
-                Ok(mut song) => {
-                    if let Some(song_object) = song.as_object_mut() {
-                        song_object
-                            .insert(String::from("station"), Value::from(args.station.as_str()));
-                        song_object
-                            .insert(String::from("time"), Value::from(Utc::now().to_rfc3339()));
-                    }
-                    if args.debug {
-                        println!("{}", serde_json::to_string_pretty(&song)?);
-                    }
-                    if let Some(endpoint) = args.endpoint.as_ref() {
-                        match client.post(endpoint).json(&song).send().await {
-                            Ok(response) => {
-                                if args.debug {
-                                    println!("Endpoint response: {}", response.status())
-                                }
-                            }
-                            Err(e) => {
-                                eprintln!("Endpoint error: {e}");
+        let signature = SignatureGenerator::make_signature_from_file(&args.stream_file)
+            .map_err(|e| anyhow!("{}", e))?;
+        match recognize_song_from_signature(&signature) {
+            Ok(mut song) => {
+                if let Some(song_object) = song.as_object_mut() {
+                    song_object.insert(String::from("station"), Value::from(args.station.as_str()));
+                    song_object.insert(String::from("time"), Value::from(Utc::now().to_rfc3339()));
+                }
+                if args.debug {
+                    println!("{}", serde_json::to_string_pretty(&song)?);
+                }
+                if let Some(endpoint) = args.endpoint.as_ref() {
+                    match client.post(endpoint).json(&song).send().await {
+                        Ok(response) => {
+                            if args.debug {
+                                println!("Endpoint response: {}", response.status())
                             }
                         }
+                        Err(e) => {
+                            eprintln!("Endpoint error: {e}");
+                        }
                     }
-                    chunks.clear();
                 }
-                Err(e) => {
-                    eprintln!("Recognize error: {e}");
-                }
-            },
+                chunks.clear();
+            }
             Err(e) => {
-                eprintln!("Signature error: {e}");
+                eprintln!("Recognize error: {e}");
             }
         }
     }
     Ok(())
+}
+
+fn main() -> anyhow::Result<()> {
+    let args: Args = argh::from_env();
+    println!("Starting...");
+    let client = Client::builder()
+        .user_agent(APP_USER_AGENT)
+        .timeout(Duration::from_secs(30))
+        .pool_max_idle_per_host(0)
+        .build()?;
+    let stream_client = Client::builder()
+        .http2_keep_alive_while_idle(true)
+        .pool_idle_timeout(None)
+        .pool_max_idle_per_host(usize::MAX)
+        .build()?;
+    let runtime = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()?;
+    loop {
+        runtime.block_on(async {
+            if let Err(e) = start(&args, &client, &stream_client).await {
+                eprintln!("Error occurred: {e}");
+            }
+        });
+    }
 }
